0945b55bd406808bc56c8fe6da113ef806b7dc55
yong-jie <viorama@gmail.com>
2019-06-08 17:22:56 +0800

    Abstract source acquisition code

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,21 @@
 extern crate rodio;
 
+use rodio::decoder::Decoder;
+use std::fs::File;
 use std::io::BufReader;
 
 pub fn play_music(filename: &str) -> rodio::Sink {
     let device = rodio::default_output_device().unwrap();
     let sink = rodio::Sink::new(&device);
-    let file = std::fs::File::open(filename).unwrap();
-    let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+    let source = get_source(filename);
     sink.append(source);
     sink
+}
+
+fn get_source(filename: &str) -> Decoder<BufReader<File>> {
+    let file: File = File::open(filename).unwrap();
+    let buf_reader: BufReader<File> = BufReader::new(file);
+    let source = Decoder::new(buf_reader).unwrap();
+    source
 }
 
