8cbdb632ac369f02f12bb2bb861146b5b19ccb40
yong-jie <viorama@gmail.com>
2019-06-08 17:29:56 +0800

    Refactor to use less ambiguous name

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,12 @@ use std::io::BufReader;
 pub fn play_music(filename: &str) -> rodio::Sink {
     let device = rodio::default_output_device().unwrap();
     let sink = rodio::Sink::new(&device);
-    let source = get_source(filename);
-    sink.append(source);
+    let music_source = decode_file(filename);
+    sink.append(music_source);
     sink
 }
 
-fn get_source(filename: &str) -> Decoder<BufReader<File>> {
+fn decode_file(filename: &str) -> Decoder<BufReader<File>> {
     let file: File = File::open(filename).unwrap();
     let buf_reader: BufReader<File> = BufReader::new(file);
     let source = Decoder::new(buf_reader).unwrap();
