77ca8297f14560e3e7f3ef5dcb7203b37c62e0ef
yong-jie <viorama@gmail.com>
2019-06-10 00:17:35 +0800

    Use pausable and duration in audio

--- src/audio.rs
+++ src/audio.rs
@@ -1,5 +1,7 @@
 
-use rodio::{Device, Sink};
+use rodio::{Device, Sink, Source};
+use std::time::Duration;
+use std::sync::{Arc, Mutex};
 use crate::sound_data;
 
 /// A playable audio.
@@ -17,7 +19,8 @@ pub struct Music {
     // sinks unusable.
     device: Device,
     sink: Sink,
-    sound_data: sound_data::SoundData
+    sound_data: sound_data::SoundData,
+    duration: Arc<Mutex<Duration>>
 }
 
 impl Music {
@@ -25,11 +28,17 @@ impl Music {
     pub fn new(
         device: &Device,
         sound_data: sound_data::SoundData) -> Music {
+        let duration = Arc::new(Mutex::new(Duration::from_secs(0)));
         Music {
             device: device.to_owned(),
             sink: Sink::new(&device),
-            sound_data
+            sound_data,
+            duration
         }
+    }
+
+    fn pause(&self) {
+        self.sink.pause();
     }
 }
 
@@ -37,7 +46,15 @@ impl Audio for Music {
     fn play(&self) {
         if self.sink.empty() {
             // This is the first time playing.
-            self.sink.append(self.sound_data.decoder());
+            let decoder = self.sound_data.decoder();
+
+            // Make this sound pausable.
+            let pausable = decoder.pausable(false);
+
+            // Enable tracking of time elapsed
+            let elapsed = pausable.elapsed(self.duration.clone());
+
+            self.sink.append(elapsed);
         } else if self.sink.is_paused() {
             self.sink.play();
         }
