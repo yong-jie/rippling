245db8d60eca8a363c4733309d48c8d53cc5f0cb
yong-jie <viorama@gmail.com>
2019-06-12 14:28:05 +0800

    Wrap sink and duration in RefCell for Music to be internally mutable

--- src/audio.rs
+++ src/audio.rs
@@ -1,5 +1,6 @@
 
 use rodio::{Device, Sink, Source};
+use std::cell::RefCell;
 use std::time::Duration;
 use std::sync::{Arc, Mutex};
 use crate::sound_data;
@@ -18,9 +19,9 @@ pub struct Music {
     // because rodio's stop function actually renders
     // sinks unusable.
     device: Device,
-    sink: Sink,
+    sink: RefCell<Sink>,
     sound_data: sound_data::SoundData,
-    duration: Arc<Mutex<Duration>>
+    duration: RefCell::<Arc<Mutex<Duration>>>
 }
 
 impl Music {
@@ -28,10 +29,10 @@ impl Music {
     pub fn new(
         device: &Device,
         sound_data: sound_data::SoundData) -> Music {
-        let duration = Arc::new(Mutex::new(Duration::from_secs(0)));
+        let duration = RefCell::new(Arc::new(Mutex::new(Duration::from_secs(0))));
         Music {
             device: device.to_owned(),
-            sink: Sink::new(&device),
+            sink: RefCell::new(Sink::new(&device)),
             sound_data,
             duration
         }
@@ -39,20 +40,20 @@ impl Music {
 
     /// Pauses the Music. Can be resumed with play().
     pub fn pause(&self) {
-        self.sink.pause();
+        self.sink.borrow().pause();
     }
 
     /// Stops the Music. Subsequent calls to play() will
     /// start the Music from scratch.
-    pub fn stop(&mut self) {
-        self.sink = Sink::new(&self.device);
-        self.duration = Arc::new(Mutex::new(Duration::from_secs(0)));
+    pub fn stop(&self) {
+        self.sink.replace(Sink::new(&self.device));
+        self.duration.replace(Arc::new(Mutex::new(Duration::from_secs(0))));
     }
 }
 
 impl Audio for Music {
     fn play(&self) {
-        if self.sink.empty() {
+        if self.sink.borrow().empty() {
             // This is the first time playing.
             let decoder = self.sound_data.decoder();
 
@@ -60,11 +61,11 @@ impl Audio for Music {
             let pausable = decoder.pausable(false);
 
             // Enable tracking of time elapsed
-            let elapsed = pausable.elapsed(self.duration.clone());
+            let elapsed = pausable.elapsed(self.duration.borrow().clone());
 
-            self.sink.append(elapsed);
-        } else if self.sink.is_paused() {
-            self.sink.play();
+            self.sink.borrow().append(elapsed);
+        } else if self.sink.borrow().is_paused() {
+            self.sink.borrow().play();
         }
     }
 }
